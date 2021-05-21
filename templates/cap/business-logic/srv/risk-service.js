375ddc2cd722cd76271664f23255bcdf47362981
d038359 <d038359@exchange.sap.corp>
2021-05-21 14:29:55 +0000

    Update Fri 21 May 2021 02:29:55 PM UTC

--- templates/cap/business-logic/srv/risk-service.js
+++ templates/cap/business-logic/srv/risk-service.js
@@ -2,9 +2,9 @@
  * Implementation for Risk Management service defined in ./risk-service.cds
  */
 module.exports = async (srv) => {
-    srv.after('READ', 'Risks', (risks) => {
-
-        risks.forEach((risk) => {
+    srv.after('READ', 'Risks', (risksData) => {
+        const risks = Array.isArray(risksData) ? risksData : [risksData];
+        risks.forEach(risk => {
             if (risk.impact >= 100000) {
                 risk.criticality = 1;
             } else {
