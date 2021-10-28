3de6c4395edfae67f7d1486d6c76fe6abae715eb
d038359 <d038359@exchange.sap.corp>
2021-10-28 07:52:12 +0000

    Update Thu 28 Oct 2021 07:52:12 AM UTC

---
+++ srv/risk-service.cds
@@ -0,0 +1,34 @@
+using { sap.ui.riskmanagement as my } from '../db/schema';
+
+@path: 'service/risk'
+service RiskService {
+  entity Risks @(restrict : [
+            {
+                grant : [ 'READ' ],
+                to : [ 'RiskViewer' ]
+            },
+            {
+                grant : [ '*' ],
+                to : [ 'RiskManager' ]
+            }
+        ]) as projection on my.Risks;
+    annotate Risks with @odata.draft.enabled;
+  entity Mitigations @(restrict : [
+            {
+                grant : [ 'READ' ],
+                to : [ 'RiskViewer' ]
+            },
+            {
+                grant : [ '*' ],
+                to : [ 'RiskManager' ]
+            }
+        ]) as projection on my.Mitigations;
+    annotate Mitigations with @odata.draft.enabled;
+    @readonly
+    entity Suppliers @(restrict : [
+              {
+                  grant : [ 'READ' ],
+                  to : [ 'RiskViewer', 'RiskManager' ]
+              }
+          ]) as projection on my.Suppliers;
+}
---
+++ srv/risk-service.js
@@ -0,0 +1,64 @@
+
+const cds = require('@sap/cds')
+
+/**
+ * Implementation for Risk Management service defined in ./risk-service.cds
+ */
+module.exports = cds.service.impl(async function() {
+
+    const bupa = await cds.connect.to('API_BUSINESS_PARTNER');
+
+    this.on('READ', 'Suppliers', async req => {
+        return bupa.run(req.query);
+    });
+
+    this.after('READ', 'Risks', risksData => {
+        const risks = Array.isArray(risksData) ? risksData : [risksData];
+        risks.forEach(risk => {
+            if (risk.impact >= 100000) {
+                risk.criticality = 1;
+            } else {
+                risk.criticality = 2;
+            }
+        });
+    });
+    // Risks?$expand=supplier
+    this.on("READ", 'Risks', async (req, next) => {
+        if (!req.query.SELECT.columns) return next();
+        const expandIndex = req.query.SELECT.columns.findIndex(
+            ({ expand, ref }) => expand && ref[0] === "supplier"
+        );
+        if (expandIndex < 0) return next();
+
+        // Remove expand from query
+        req.query.SELECT.columns.splice(expandIndex, 1);
+
+        // Make sure supplier_ID will be returned
+        if (!req.query.SELECT.columns.indexOf('*') >= 0 &&
+            !req.query.SELECT.columns.find(
+                column => column.ref && column.ref.find((ref) => ref == "supplier_ID"))
+        ) {
+            req.query.SELECT.columns.push({ ref: ["supplier_ID"] });
+        }
+
+        const risks = await next();
+
+        const asArray = x => Array.isArray(x) ? x : [ x ];
+
+        // Request all associated suppliers
+        const supplierIds = asArray(risks).map(risk => risk.supplier_ID);
+        const suppliers = await bupa.run(SELECT.from('RiskService.Suppliers').where({ ID: supplierIds }));
+
+        // Convert in a map for easier lookup
+        const suppliersMap = {};
+        for (const supplier of suppliers)
+            suppliersMap[supplier.ID] = supplier;
+
+        // Add suppliers to result
+        for (const note of asArray(risks)) {
+            note.supplier = suppliersMap[note.supplier_ID];
+        }
+
+        return risks;
+    });
+});
