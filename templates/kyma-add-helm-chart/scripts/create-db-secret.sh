8a8e94f358f8522bc32bd3dfe2075355208ac9e8
d038359 <d038359@exchange.sap.corp>
2022-06-23 07:13:21 +0000

    Update Thu Jun 23 07:13:21 UTC 2022

--- templates/kyma-add-helm-chart/scripts/create-db-secret.sh
+++ templates/kyma-add-helm-chart/scripts/create-db-secret.sh
@@ -4,13 +4,21 @@ set -e
 cd "$(dirname "$(dirname "$0")")"
 . ./scripts/values.sh
 
-if true-value .multitenancy.enabled; then
+if true-value 2>/dev/null .saas_registry.enabled; then
   echo >&2 "[ERROR] DB secret only required for single tenancy apps"
 fi
 
 NAME="$1"
 if [ "$NAME" == "" ]; then
-    NAME="$(value .srv.bindings.db.fromSecret)"
+  if [ ! -f "chart/values.yaml" ]; then
+    echo >&2 "[ERROR] Please either specify the name for the DB secret or maintain it in the Helm chart"
+    exit 1
+  fi
+  NAME="$(value .srv.bindings.db.fromSecret)"
+  if [ "$NAME" == "" -o "$NAME" == "<nil>" ]; then
+    echo >&2 "[ERROR] Please either specify the name for the DB secret or maintain it in the Helm chart"
+    exit 1
+  fi
 fi
 
 SECRET_HEADER="$(cat <<EOF
@@ -25,9 +33,9 @@ stringData:
 EOF
 )"
 
-cf service $NAME || cf create-service hana hdi-shared $NAME
+cf 2>/dev/null >/dev/null service $NAME || cf create-service hana hdi-shared $NAME
 while true; do
-    STATUS="$(cf service $NAME | grep status:)"
+    STATUS="$(cf 2>/dev/null service $NAME | grep status: | head -n 1)"
     echo $STATUS
     if [[ "$STATUS" = *succeeded* ]]; then
         break
@@ -38,4 +46,10 @@ done
 cf create-service-key $NAME $NAME-key
 
 node "$(dirname "$0")/format-kyma-secret.js" -- "$(echo "$SECRET_HEADER")" "$(cf service-key $NAME $NAME-key)" | kubectl apply -f -
+echo
+echo "HANA DB Kubernetes secret '$NAME' created."
+echo
+echo "You can view it using:"
+echo
+echo "kubectl get secret $NAME -o yaml"
 exit 0
